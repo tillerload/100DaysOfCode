0b47e502f022f529d8268c1746abc19d4d7175b7
tillerload <tyler.george.work@gmail.com>
2023-05-03 21:38:15 -0230

    started calculator but its sleepy time zzz

--- day1/app.js
+++ day1/app.js
@@ -1,1 +1,15 @@
-console.log('hey')
+const tipCalc = (total, percent) => {
+  let tip = total * percent
+  console.log(tip)
+}
+
+
+const btnClick = () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let total = document.querySelector('#total').value
+    let percent = document.querySelector('#percent').value
+    tipCalc(total, percent)
+  })
+  
+}
