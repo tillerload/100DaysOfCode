4bc2eb0bf9ad1bccdd5a11d742ece075164541bb
tillerload <tyler.george.work@gmail.com>
2023-05-15 19:25:24 -0230

    finished code for day 2 which is actually day 4 now lol

--- day2/app.js
+++ day2/app.js
@@ -1,6 +1,26 @@
-let btnEl = document.getElementsByClassName('btn')
+// Selecting Queries and storing them in variables
+const btnEl = document.querySelector('button')
+const body = document.querySelector('body')
 
-btnEl.addEventListener('click', () => {
-  console.log('jey')
-})
+// setting a default background colour
+body.style.backgroundColor = 'white'
+
+// creating an array of colours
+const colourList = [
+  'purple',
+  'blue',
+  'red',
+  'green',
+  'orange',
+  'yellow',
+]
+
+// function to randomly select a colour from the array and change the background colour
+const buttonClick = () => {
+  let choice = colourList[Math.floor(Math.random() * colourList.length)];
+  body.style.backgroundColor = choice;
+}
+
+// adding an event listener
+btnEl.addEventListener('click', buttonClick)
 
